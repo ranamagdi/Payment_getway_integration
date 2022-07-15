3a8d0e577a629ec7e6c4f7cd319cd6bf1c0c8bf9
ranamagdi <ranagrida@gmail.com>
2022-07-16 00:29:15 +0200

    before getway

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
       
-      
+      function showDiv() {
+        let div1 = document.querySelector('#first');
+        let div2 = document.querySelector('#next');
+    
+        if (div1.style.display == "block") {
+            div1.style.display = "none";
+            div2.style.display = "block";
+        } else {
+            div1.style.display = "block";
+            div2.style.display = "none";
+        }
+    
+     }
+     
+     function showDiv1() {
+        let div1 = document.querySelector('#first');
+        let div2 = document.querySelector('#next');
+    
+        if (div2.style.display == "block") {
+            div2.style.display = "none";
+            div1.style.display = "block";
+        } else {
+            div2.style.display = "block";
+            div1.style.display = "none";
+        }
+    
+     }
+     
